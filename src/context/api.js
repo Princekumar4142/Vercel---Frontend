15a732d02da161e261c8842e7b954756ac2ff811
Prince kumar <princebth1988@gmail.com>
2026-07-05 19:20:33 +0530

    Update backend

--- src/context/api.js
+++ src/context/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://vercel-backend-pk01.vercel.app/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://vercel-backend-533t.onrender.com'
 })
 
 api.interceptors.request.use((config) => {
